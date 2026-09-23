b79b334c212e8923fee70aaeaf6dbc4bf8c01f3a
Cristian <cchura.cpc@gmail.com>
2026-09-22 21:12:37 -0400

    Grupos rutas auth

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get("/saludo", function(){
+    return "Hola saludos desde api.php";
+});
+
+// autenticación
+Route::prefix('/v1/auth')->group(function () {
+
+    Route::post("/login", function(){
+        return "Iniciado sesion...";
+    });
+    Route::post("/register", function(){
+        return "Registrando...";
+    });
+
+    Route::middleware(['auth'])->group(function(){
+
+        Route::get("/profile", function(){
+            return "Mostrando mi Perfil...";
+        });
+        Route::post("/logout", function(){
+            return "Cerrando sesion...";
+        });
+    });
+
+});
+
+Route::get('no-autorizado', function(){
+    return ["mensaje" => "No estas permitido para ver esta pagina"];
+})->name('login');
+
