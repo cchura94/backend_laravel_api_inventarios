bf2b8330f8a68ef3c9c299d60798af52fc38e8d5
Cristian <cchura.cpc@gmail.com>
2026-09-24 21:12:48 -0400

    perfil, logout + registro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
@@ -13,10 +15,10 @@ class AuthController extends Controller
         $credenciales = $request->validate([
             "email" => "required|email|min:5|max:200",
             "password" => ["required", Password::min(6)       // Mínimo 8 caracteres
-                                                ->mixedCase()      // Al menos una mayúscula y una minúscula
-                                                ->letters()        // Al menos una letra
-                                                ->numbers()        // Al menos un número
-                                                ->symbols()        // Al menos un carácter especial (!@#$%^&*)
+                                                // ->mixedCase()      // Al menos una mayúscula y una minúscula
+                                                // ->letters()        // Al menos una letra
+                                                // ->numbers()        // Al menos un número
+                                                // ->symbols()        // Al menos un carácter especial (!@#$%^&*)
                                                 ]
         ]);
 
@@ -39,15 +41,29 @@ class AuthController extends Controller
 
     public function register(Request $request){
         // validacion
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required"
+        ]);
         // registro
+        $usuario = new User();
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = Hash::make($request->password);
+        $usuario->save();
         // respuesta
+
+        return response()->json(["mensaje" => "Usuario Registrado"]);
     }
 
     public function profile(Request $request){
-
+        return response()->json($request->user(), 200);
     }
 
     public function logout(Request $request){
+        $request->user()->tokens()->delete();
 
+        return response()->json([ "mensaje" => "Logout" ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::prefix('/v1/auth')->group(function () {
     Route::post("/login", [AuthController::class, "login"]);
     Route::post("/register", [AuthController::class, "register"]);
 
-    Route::middleware(['auth'])->group(function(){
+    Route::middleware(['auth:sanctum'])->group(function(){
 
         Route::get("/profile", [AuthController::class, "profile"]);
         Route::post("/logout", [AuthController::class, "logout"]);
